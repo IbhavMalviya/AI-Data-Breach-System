71356628dce41d9412720a4d7ff5f29ecdca0ec7
Ibhav <ibhavmalviya@gmail.com>
2025-07-06 15:22:39 +0530

    Final Changes

--- src/simulate_stream.py
+++
@@ -1,9 +0,0 @@
-# simulate_stream.py
-import pandas as pd
-import time
-
-def stream_data(csv_path, chunk_size=1, delay=1):
-    df = pd.read_csv(csv_path)
-    for i in range(0, len(df), chunk_size):
-        yield df.iloc[i:i+chunk_size]
-        time.sleep(delay)
